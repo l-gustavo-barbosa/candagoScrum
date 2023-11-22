4b175c4375c8c01042e1afd453fa08d77d88dcbf
Gustavo <gustavosbpereira@outlook.com>
2023-11-22 09:33:10 -0300

    -m "formatando o código e finalizando arquivos"

---
+++ pagina-leitura/capitulo-2/exercicio/index.js
@@ -0,0 +1,41 @@
+//seleciona o elemento de id "form", adiciona um eventListener que ao identificar um evento "submit" inicia uma função.
+document.getElementById("form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Evitar o comportamento padrão do formulário
+    //armazena as respostas selecionando-as pelo formulario/nome dos inputs
+    const repostas = [document.forms['exercicios']['Pergunta1'].value, document.forms['exercicios']['Pergunta2'].value,
+    document.forms['exercicios']['Pergunta3'].value, document.forms['exercicios']['Pergunta4'].value, document.forms['exercicios']['Pergunta5'].value]
+    //inicia função "avalia" e passa as respostas como parametro
+    avalia(repostas);
+});
+var i = 0;
+document.getElementById("topDown").addEventListener("click", function(){
+    i++;
+   if(i % 2){
+    document.getElementById("nav").classList.remove('inativo');
+   }else {document.getElementById("nav").classList.add('inativo');}
+})
+function avalia(respostas){
+    //armazena o gabarito dos exercicios
+    let gabarito = ['B','C','B','C','D'];
+    //inicia a variavel que armazena a nota
+    var nota = 0;
+    //inicia o laço de repetição que verifica as respostas
+    for(i = 0; i < respostas.length; i++){
+        //verifica se a resposta da pergunta de valor i bate com a resposta de valor i
+        if(gabarito[i] == respostas[i]){
+            //adiciona 1 ponto na nota
+            nota++;
+        } else {console.log("Errou a questão " + (1 + i))}
+    }
+    //verifica se a nota é maior que a média
+    if(nota >= (gabarito.length / 2)){
+        //redireciona o usuario para a proxima pagina ou para o certificado
+        location.href = "../../../pagina-leitura/capitulo-3/"
+    } else {window.alert("Nota insuficiente. Por favor, refaça o teste.")}
+
+}
+//seleciona o elemento de id "prevPage", adiciona um eventListener que ao identificar um evento "click" inicia uma função
+document.getElementById("prevPage").addEventListener("click", function(){
+    //volta uma etapa no histórico do navegador
+    history.back();
+})
--- pagina-leitura/capitulo-2/index.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("nextPage").addEventListener("click", function(){
-    location.href = "../exercicios/";
-})
-
-document.getElementById("prevPage").addEventListener("click", function(){
-    history.back();
-})
