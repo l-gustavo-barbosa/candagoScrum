f2eb97c48052d241e3d4efeebef5beafd5dd54a8
caique moura <caique.fred@outlook.com>
2023-11-22 08:04:13 -0300

    finalização cap2, exercicios cap3 e imagem cap 1

--- pagina-leitura/capitulo-2/index.js
+++ pagina-leitura/capitulo-2/index.js
@@ -1,7 +1,7 @@
-document.getElementById("nextPage").addEventListener("click", function(){
+document.getElementById("nextPage").addEventListener("click", function () {
     location.href = "../exercicios/";
 })
 
-document.getElementById("prevPage").addEventListener("click", function(){
+document.getElementById("prevPage").addEventListener("click", function () {
     history.back();
 })
